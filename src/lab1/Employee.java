9abec973a554b2f7b99879a28fafc20aaea6e9f9
Isaac <Isaac@192.168.160.1>
2017-08-31 15:30:08 -0500

    Add getters and setters

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -22,6 +22,80 @@ public class Employee {
     private String cubeId;
     private Date orientationDate;
 
+    //Getters
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
+
+    //Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+    
+    
 
 }
